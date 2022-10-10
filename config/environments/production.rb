9f3c323c1a005556a2fcee75b87305c240645cd9
sakoji14 <sj.yu.sato1424@gmail.com>
2022-10-10 21:18:19 +0900

    本番環境メール

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,22 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'https://original-app-2580.herokuapp.com/' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default :charset => "utf-8"
+  # Gmail の場合
+  config.action_mailer.smtp_settings = {
+  :enable_starttls_auto => true,
+  :address => "smtp.gmail.com",
+  :port => 587,
+  :domain => 'smtp.gmail.com',
+  :user_name => ENV['GMAIL_ADDRESS'],
+  :password => ENV['GMAIL_PASSWORD'],
+  :authentication => 'login'
+}
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
