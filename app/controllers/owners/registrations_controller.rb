75a872f00f3fb03893950fdb6b883b08e5cd09c0
sakoji14 <sj.yu.sato1424@gmail.com>
2022-11-15 17:13:25 +0900

    Update registrations_controller.rb

--- app/controllers/owners/registrations_controller.rb
+++ app/controllers/owners/registrations_controller.rb
@@ -2,7 +2,7 @@
 
 class Owners::RegistrationsController < Devise::RegistrationsController
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  before_action :basic_auth
   
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
@@ -69,7 +69,11 @@ class Owners::RegistrationsController < Devise::RegistrationsController
     params.require(:owner).permit(:corporation, :nickname, :tell, :pref_id, :appeal, :image)
   end
 
-  
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:corporation, :nickname, :tell, :pref_id, :appeal, :image])
